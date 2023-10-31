829d23319098820d8ef42368c289bc1195664436
WhyBusyy <ybg6152@naver.com>
2023-10-31 16:02:51 +0900

    11일차 map

---
+++ 2.JS/1.basic/map.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5];
+
+const sqrNumbers = numbers.map((num) => num * num);
+// function square(num) {
+//     return num * num;
+// }
+
+console.log(numbers);
+console.log(sqrNumbers);

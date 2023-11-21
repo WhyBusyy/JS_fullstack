50ca4f6985c90a38eb08e9387132aabb7b77d80a
WhyBusyy <ybg6152@naver.com>
2023-11-21 18:43:35 +0900

    26일차 miniHW2 수정 - 테이블 헤더 데이터 파싱 후 데이터 추가

--- 3.NODE/5.exp-func/1.template/4.miniHW2/app.js
+++ 3.NODE/5.exp-func/1.template/4.miniHW2/app.js
@@ -6,7 +6,6 @@ const csv = require("csv-parser"); //선택사항
 const app = express();
 const port = 3000;
 
-const results = [];
 
 app.use(express.static("public"));
 //눈적스 초기화
@@ -16,18 +15,32 @@ nunjucks.configure("views", {
 });
 app.set("view engine", "html");
 
+app.use((req, res, next) => {
+  // 모든 요청이 있을 때마다 미들웨어를 거쳐간다.. next()
+  const start = Date.now();
+  //나중에 동작할 리스너 등록
+  res.on('finish', () => {
+    const end = Date.now();
+    const duration = end - start;
+    console.log(`요청 ${req.path}에서 소요시간 ${duration}ms입니다.`);
+  })
+  next();
+})
+
 app.get("/", (req, res) => {
+  const results = [];
+  const hdResults = [];
   try {
     fs.createReadStream(
         "/Users/yubyung-gyu/SESAC/me/sesac_ot/3.NODE/generator/1.user/User.csv"
       )
       .pipe(csv())
+      .on('headers',(headers) => hdResults.push(...headers))
       .on("data", (data) => results.push(data))
       .on("end", () => {
         const limitedResults = results.slice(0, 20);
-        res.render("index", { data : limitedResults });
+        res.render("index", { header : hdResults, data : limitedResults });
       });
-
   } catch (error) {
     console.error("에러 발생:", error);
     res.status(500).send("Internal Server Error");

c45ed147fcf688b015a64c03871d30a2a3b78e7b
WhyBusyy <ybg6152@naver.com>
2023-11-22 20:36:01 +0900

    27일차 쿠키 개념 이해

---
+++ 3.NODE/3.express/5.session/2.cookie/cookie.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    // 요청 안의 정보 출력
+    console.log(req.url, req.headers.cookie);
+    // 응답의 헤더 정보 (resp code, message)
+    res.writeHEad(200, 'OK');
+    // 응답의 바디를 보낸 것
+    res.end('DONE');
+})
+
+server.listen(3000, () => {
+    console.log('서버 오픈');
+})

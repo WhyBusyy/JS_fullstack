d58452c1a4ca9c0a95af2a711f9805ac5bf3a3e4
WhyBusyy <ybg6152@naver.com>
2023-11-25 21:05:23 +0900

    cart2 리팩토링 1차(프론트JS)

---
+++ 3.NODE/3.express/5.session/6.cart3/public/static/product.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/api/products")
+    .then((response) => response.json())
+    .then((products) => displayProduct(products));
+  loginStatus();
+});
+
+function displayProduct(products) {
+  const productTableBody = document.querySelector("#productTable tbody");
+  products.forEach((product) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>${product.id}</td>
+        <td>${product.name}</td>
+        <td>${product.price}</td>
+        <td><button onclick="addToCart(${product.id})">담기</button></td>
+        `;
+    productTableBody.appendChild(row);
+  });
+}
+
+function addToCart(productId) {
+  fetch(`/api/cart/${productId}`, { method: "POST" })
+  .then((response) => response.json())
+  .then((data) => {
+    // alert(JSON.stringify(data.cart));
+    alert(data.message);
+  });
+  fetch("../cart")
+    .then((response) => response.json())
+    .then(() => {
+      window.location.href = "/cart";
+    });
+}
+
+function loginStatus() {
+  fetch("/api") // 백엔드 구현: 사용자 세션 있으면 username 반납
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.username) {
+        document.getElementById("userLog").style.display = "block";
+        document.getElementById("username").innerText = data.username;
+      }
+    });
+}
+
+function logout() {
+  fetch("/logout")
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data.message);
+      showLoginForm();
+    });
+}

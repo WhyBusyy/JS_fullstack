1cff60ecc18a6311ce717a5d6313047f17076c2a
WhyBusyy <ybg6152@naver.com>
2023-11-25 21:03:48 +0900

    cart2 리팩토링 1차(data)

---
+++ 3.NODE/3.express/5.session/6.cart3/src/data/products.js
@@ -0,0 +1,8 @@
+const products = [
+  { id: 1, name: "coke", price: 2000 },
+  { id: 2, name: "soda", price: 3000 },
+  { id: 3, name: "sprite", price: 2000 },
+  { id: 4, name: "coffee", price: 4000 },
+];
+
+module.exports = products;
